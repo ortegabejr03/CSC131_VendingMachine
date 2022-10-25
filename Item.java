1ffea5c6d29ae06a1db307bdcfa80fb8fc624eaa
ortegabejr03 <gabriel.ortegajr03@gmail.com>
2022-10-25 15:02:11 -0700

    Added item class

---
+++ Item.java
@@ -0,0 +1,33 @@
+public class Item {
+   private String name;
+   private int price;
+   private int quantity;
+
+   public Item(String name, int price){
+    this.name = name;
+    this.price = price;
+    quantity = 0;
+   }
+public String getName() {
+    return name;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public int getPrice() {
+    return price;
+}
+public void setPrice(int price) {
+    this.price = price;
+}
+public int getQuantity() {
+    return quantity;
+}
+public void addQuantity(int quantity) {
+    this.quantity += quantity;
+}
+
+public void removeQuantity(int quantity){
+    this.quantity -= quantity;
+}
+}
