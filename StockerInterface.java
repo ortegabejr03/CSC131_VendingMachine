83edef24f91e0fda26d8dee12b1f01a1016b8f65
ortegabejr03 <gabriel.ortegajr03@gmail.com>
2022-12-05 14:37:55 -0800

    Added stocker StockerInterface

---
+++ StockerInterface.java
@@ -0,0 +1,11 @@
+public class StockerInterface {
+    
+    //Display welcome message for stocker interface
+    public static void displayWelcome(){
+        System.out.println("Welcome Mr./Mrs stocker ... What would you like to do today?");
+        System.out.println("1. Restock an item");
+        System.out.println("2. Removing an item");
+        System.out.println("3. Creating and Inserting an item");
+        System.out.println("4. Exit the stocker interface");
+    }
+}
